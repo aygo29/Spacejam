489a774ff4ac3e475e8736e419948167ca52e149
aygo29 <72601669+aygo29@users.noreply.github.com>
2021-02-27 19:17:16 +0530

    Update Pandemic Simulation.py

--- Pandemic Simulation.py
+++ Pandemic Simulation.py
@@ -1,36 +1,49 @@
 import pygame
 import random,math
+
+infects = 1
+dead = 0
+suscepts = 0
+recovs = 0
 maxv=20
-transmissionrate=0.6
+transmissionrate=0.25
 mortality_rate = 0.03
-recchance=0.65
+recchance=0.97
 class People():
-    colour_code={"susceptible":"yellow","infected":"red","recovered":"green","dead":"black"}
+    colour_code={"susceptible":"grey","infected":"red","recovered":"green","dead":"black"}
     def __init__(self,x,y,category,sensible):
+
         self.x=x
         self.y=y
         self.category=category
         self.sensible=sensible
         self.xv=self.yv=0
         self.timesick=0
-        self.radius=3.5
-        self.rectime=random.randint(50,200)
+        self.radius=3
+        self.rectime=random.randint(50,100)
         noconstraint=True
         if noconstraint:
             self.xv=random.uniform(-maxv,maxv)
             self.yv=random.uniform(-maxv,maxv)
 
-    def move(self):
-        self.x=self.x+self.xv
-        self.y=self.y+self.yv
+    def move(self,category):
+        if self.category!="dead" and not self.sensible:
+            self.x=self.x+self.xv
+            self.y=self.y+self.yv
+        if self.category!="dead" and self.sensible:
+            self.x=self.x+self.xv/5
+            self.y=self.y+self.yv/5
     def draw(self,screen):
         pygame.draw.circle(screen,pygame.Color(self.colour_code[self.category]),(round(self.x),round(self.y)),self.radius)
     def updation(self,screen,peoples):
-        self.move()
+        global infects
+        self.move(self.category)
         if self.category=="infected":
             self.timesick+=1
             if self.timesick==self.rectime and random.random()<=recchance:
                 self.category="recovered"
+            if self.timesick == self.rectime and random.random() >= recchance:
+                self.category="dead"
         self.wallcollision(screen)
         for other in peoples:
             if self!=other:
@@ -39,10 +52,12 @@ class People():
                     if self.category=="infected" and other.category=="susceptible":
                         if random.random()<=transmissionrate:
                             other.category="infected"
+                            infects+=1
                     if self.category=="susceptible" and other.category=="infected":
                         if random.random() <= transmissionrate:
                             self.category="infected"
-
+                            infects+=1
+        return infects
 
     def wallcollision(self,screen):
         if self.x+self.radius>=screen.get_width() and self.xv>0:
@@ -69,10 +84,3 @@ class People():
         elif other.sensible:
             self.xv*=-1
             self.yv*=-1
-
-
-
-
-
-
-
