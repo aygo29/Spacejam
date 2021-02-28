3f4aa0ab1246f2a7ec03c82bd51260ed1c66096f
aygo29 <72601669+aygo29@users.noreply.github.com>
2021-02-28 10:13:33 +0530

    Update Program2.py

--- Program2.py
+++ Program2.py
@@ -1,54 +1,52 @@
 import pygame, random, time
-import numpy as np
 import pylab as plt
 from model1 import People
+from module2 import People1
 
 k = 1
 
 
 def main():
     list=animate()
-    print (list)
-    if len(list)==14:
-        graphing(list)
+    l23=animate2()
+    if len(list)==20 and len(l23)==20:
+        graphing(list,l23)
 
 
 
-def graphing(list):
-    x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-    y=[1,2,3,4,5,6,7]
+def graphing(list,l23):
 
 
-    t = [i for i in range(1, 15)]
+    t = [i for i in range(1, 21)]
     plt.figure('Graph 1')
     plt.xlabel('Time')
     plt.ylabel('Active Cases')
     plt.title('Spread of the Virus')
     plt.plot(t, list, 'r-', label='No precautions taken')
-    #plt.plot(t, cases2, 'b-', label='With precautions')
+    plt.plot(t, l23, 'b-', label='With precautions')
     plt.legend(loc='upper right')
     plt.interactive(False)
     plt.show()
 
 def animate():
-    t0 = time.clock()
+    t0=time.process_time()
     t = 0
     l = []
     global k
     pygame.init()
     WIDTH = HEIGHT = 800
     screen = pygame.display.set_mode([WIDTH, HEIGHT])
-    pygame.display.set_caption("Virus Simulation")
+    pygame.display.set_caption("Virus Simulation without precautions (grey is susceptible, red infected, green is recovered, black is dead)")
     screen.fill(pygame.Color("white"))
     p0 = People(random.randint(5, WIDTH - 5), random.randint(5, HEIGHT - 5, ), "infected", False)
     no_p = 350 - 1
     peoples = [p0]
 
     for i in range(no_p):
-        if i < no_p / 2:
+        if i < no_p / 4:
             peoples.append(People(random.randint(5, WIDTH - 5), random.randint(5, HEIGHT - 5, ), "susceptible", False))
         else:
-            peoples.append(People(random.randint(5, WIDTH - 5), random.randint(5, HEIGHT - 5, ), "susceptible", True))
+            peoples.append(People(random.randint(5, WIDTH - 5), random.randint(5, HEIGHT - 5, ), "susceptible", False))
 
     clock = pygame.time.Clock()
     fps = 30
@@ -57,6 +55,8 @@ def animate():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 runtime = False
+            if len(l)==19:
+                runtime=False
 
         for i in range(no_p):
             k = peoples[i].updation(screen, peoples)
@@ -69,8 +69,57 @@ def animate():
 
         clock.tick(fps)
 
-        t1 = time.clock()
-        if round(t1 - t0) % 5 == 0 and t != round(t1 - t0) and len(l)<=13:
+
+        t1=time.process_time()
+        if round(t1 - t0) % 3 == 0 and t != round(t1 - t0) and len(l)<=19:
+            l.append(k)
+            t = round(t1 - t0)
+    pygame.quit()
+
+
+    return l
+
+def animate2():
+    t0=time.process_time()
+    t = 0
+    l = []
+    global k
+    pygame.init()
+    WIDTH = HEIGHT = 800
+    screen = pygame.display.set_mode([WIDTH, HEIGHT])
+    pygame.display.set_caption("Virus Simulation with precautions  (grey is susceptible, red infected, green is recovered, black is dead)")
+    screen.fill(pygame.Color("white"))
+    p0 = People1(random.randint(5, WIDTH - 5), random.randint(5, HEIGHT - 5, ), "infected", False)
+    no_p = 350 - 1
+    peoples = [p0]
+
+    for i in range(no_p):
+        if i < no_p / 20:
+            peoples.append(People1(random.randint(5, WIDTH - 5), random.randint(5, HEIGHT - 5, ), "susceptible", False))
+        else:
+            peoples.append(People1(random.randint(5, WIDTH - 5), random.randint(5, HEIGHT - 5, ), "susceptible", True))
+
+    clock = pygame.time.Clock()
+    fps = 30
+    runtime = True
+    while runtime:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                runtime = False
+
+        for i in range(no_p):
+            k = peoples[i].updation1(screen, peoples)
+        screen.fill(pygame.Color("white"))
+        for i in range(no_p):
+            peoples[i].draw1(screen)
+        # t1 = time.clock()
+        # p0.draw(screen)
+        pygame.display.flip()
+
+        clock.tick(fps)
+
+        t1 = time.process_time()
+        if round(t1 - t0) % 3 == 0 and t != round(t1 - t0) and len(l)<=19:
             l.append(k)
             t = round(t1 - t0)
     pygame.quit()
@@ -79,3 +128,4 @@ def animate():
     return l
 
 main()
+
